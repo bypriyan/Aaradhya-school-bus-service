34822ec0dca23464e28d4b7b3b6f5b19c30aaa8d
chintukumar111 <chintudas499@gmail.com>
2025-02-19 22:23:15 +0530

    only fronted

--- app/src/main/java/com/bypriyan/aaradhyaschoolbusservice/activity/SignUpActivity.kt
+++ app/src/main/java/com/bypriyan/aaradhyaschoolbusservice/activity/SignUpActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bypriyan.aaradhyaschoolbusservice.R
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
