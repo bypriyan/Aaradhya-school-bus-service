7991195691575e2965091320f3f572bceb04ef26
priyanshu <104priyanshu@gmail.com>
2025-02-26 20:16:22 +0530

    new marker added

--- app/src/main/java/com/bypriyan/aaradhyaschoolbusservice/activity/DasboardActivity.kt
+++ app/src/main/java/com/bypriyan/aaradhyaschoolbusservice/activity/DasboardActivity.kt
@@ -35,7 +35,9 @@ class DasboardActivity : AppCompatActivity() {
         token_type = getIntent().getStringExtra(Constants.KEY_TOKEN_TYPE).toString()
 
         Log.d("dash", "onCreate: $userId, $token, $token_type")
+
         userViewModel.getUserDetails(userId, token)
+
         userViewModel.userDetails.observe(this) { userDetails ->
             // Update UI with user details
             binding.name.text = "Hi, ${userDetails.fullName}"
@@ -44,7 +46,7 @@ class DasboardActivity : AppCompatActivity() {
 
         binding.txtPickupDropSameLocation.setOnClickListener {
             // Start PickupDropActivity for same pickup and drop location
-            val intent = Intent(this, PaymentOptionActivity::class.java)
+            val intent = Intent(this, PickupDropActivity::class.java)
             intent.putExtra("MODE", "SAME_LOCATION") // Flag for same location
             startActivity(intent)
         }
