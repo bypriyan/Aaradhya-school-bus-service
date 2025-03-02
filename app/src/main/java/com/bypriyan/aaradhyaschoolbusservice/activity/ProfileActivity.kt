9865f0d8a926cddd92282feec3693d71f666ad3e
priyanshu <104priyanshu@gmail.com>
2025-03-02 23:03:06 +0530

    done activity

---
+++ app/src/main/java/com/bypriyan/aaradhyaschoolbusservice/activity/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.bypriyan.aaradhyaschoolbusservice.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bypriyan.aaradhyaschoolbusservice.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+}
